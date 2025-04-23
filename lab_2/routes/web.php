cbd64f3aa3515c9240b05c155c6b6b3774888502
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2025-04-23 14:20:07 +0200

    lab_2 init

--- lab_2/routes/web.php
+++ lab_2/routes/web.php
@@ -7,13 +7,18 @@ use App\Http\Controllers\PostController;
 //     return view('welcome');
 // });
 
-Route::get("/", [PostController::class, "index"])->name("posts.index");
+Route::redirect("/", "/posts");
 
 Route::prefix("/posts")->group(function () {
+    Route::get("/", [PostController::class, "index"])->name("posts.index");
     Route::get("/create", [PostController::class, "create"])->name("posts.create");
     Route::post("/", [PostController::class, "store"])->name("posts.store");
-    Route::get("/{id}", [PostController::class, "show"])->name("posts.show");
-    Route::get("/{id}/edit", [PostController::class, "edit"])->name("posts.edit");
-    Route::put("/{id}", [PostController::class, "update"])->name("posts.update");
-    Route::delete("/{id}", [PostController::class, "destroy"])->name("posts.destroy");
+    Route::get("/{id}", [PostController::class, "show"])->where('id', '[0-9]+')->name("posts.show");
+    Route::get("/{id}/edit", [PostController::class, "edit"])->where('id', '[0-9]+')->name("posts.edit");
+    Route::put("/{id}", [PostController::class, "update"])->where('id', '[0-9]+')->name("posts.update");
+    Route::delete("/{id}", [PostController::class, "destroy"])->where('id', '[0-9]+')->name("posts.destroy");
+});
+
+Route::fallback(function () {
+    abort(404);
 });
